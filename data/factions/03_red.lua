e83a700f11e67fdcd5a4c2d0a1f27d32d576c3c7
radius75 <radius75@interia.pl>
2012-10-02 23:01:21 +0200

    +factions

--- data/factions/03_red.lua
+++ data/factions/03_red.lua
@@ -1,19 +1,19 @@
-local f = Faction:new('Red')
-	:description_short('Not Blue')
-	:description('The RED team are not Blue')
+local f = Faction:new('Czerwoni')
+	:description_short('Nie Niebiescy')
+	:description('CZERWONA drużyna nie jest Niebieska')
 	:govtype('EMPIRERULE')
 	:homeworld(4,-9,-16,0,16)
 	:foundingDate(3150.0)
 	:expansionRate(3.0)
-	:military_name('Red Fleet')
-	:police_name('Red Right Hands')
+	:military_name('Czerwona Flota')
+	:police_name('Czerwona Prawica')
 	:colour(1.0,0.4,0.4)
 
-f:illegal_goods_probability('LIQUOR',88)	-- independent/empire
+f:illegal_goods_probability('LIQUOR',12)	-- independent/empire
 f:illegal_goods_probability('HAND_WEAPONS',50)	-- empire/etc
-f:illegal_goods_probability('BATTLE_WEAPONS',100)	--empire/etc
-f:illegal_goods_probability('NERVE_GAS',90)--empire
+f:illegal_goods_probability('BATTLE_WEAPONS',0)	--empire/etc
+f:illegal_goods_probability('NERVE_GAS',10)--empire
 f:illegal_goods_probability('NARCOTICS',50)--empire
-f:illegal_goods_probability('SLAVES',94)--empire
+f:illegal_goods_probability('SLAVES',6)--empire
 
-f:add_to_factions('Red')
+f:add_to_factions('Czerwoni')
